0727571376c97f6a47ec1c666430ef9a5baeebe3
Rajkumar <bhootlarajkumar@gmail.com>
2026-02-23 19:03:35 +0530

    update

--- config.py
+++
@@ -1,8 +0,0 @@
-URL = "https://www.amazon.com/Samsung-Smartphone-Unlocked-Res-Camera-Warranty/dp/B0FG1THCD7"
-
-TARGET_PRICE = 56000
-CHECK_INTERVAL = 3600 
-
-FROM_EMAIL = "rajkumaroffi03@gmail.com"
-FROM_PASSWORD = "dvvtlbftujukysgm"
-TO_EMAIL = "bhootlarajkumar@gmail.com"
